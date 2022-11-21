a79c6144fc6a905974119f8f360c8eef622dd803
Jonah <jonahcole@yahoo.com>
2022-11-21 14:13:55 -0800

    Add model for book

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryMVC.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+    }
+}
